9a6d53f0ec68d608fe5c391143a362816036a899
NesterovYehor <134618795+NesterovYehor@users.noreply.github.com>
2023-06-12 19:14:15 +0200

    Add files via upload

---
+++ ToDoPage/TodoPage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoPage
+//
+//  Created by Егор  on 05.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
